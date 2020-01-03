95d23cadaf1ed4514081fd84d6e4fa2aa59325bf
MichaelNTU <mike.omoyibo@gmail.com>
2020-01-03 13:45:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7pjss6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7pjss6)
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule,
+      HttpModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: []
+})
+export class AppModule { }
