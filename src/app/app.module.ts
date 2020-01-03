59b663f72624c164ced04f7ce68b3fd923b099f1
MichaelNTU <mike.omoyibo@gmail.com>
2020-01-03 13:49:59 +0000

    1 file modified hello component deleted from file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+//import { HelloComponent } from './hello.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 
@@ -11,7 +11,7 @@ import { HttpModule } from '@angular/http';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule,
       HttpModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, ],//HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: []
 })
